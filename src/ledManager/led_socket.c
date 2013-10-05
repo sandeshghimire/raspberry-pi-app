6436edd015e479dc09cfb9176045addcebb9cd17
sandesh ghimire <sandeshghimire@outlook.com>
2013-10-04 17:16:36 -0700

    socket handler

--- src/ledManager/led_socket.c
+++ src/ledManager/led_socket.c
@@ -1,11 +1,31 @@
+/*******************************************************************************
+ * copyright 2013 sandesh ghimire
+ *
+ * filename: led_socket.c
+ *
+ * description: Socket utility function
+ *
+ ******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/un.h>
+#include <syslog.h>
+#include "led_priv.h"
 
-
+/*******************************************************************************
+ * function: get_led_socket
+ *
+ * description: open and return socket handler
+ *
+ ******************************************************************************/
 
 int get_led_socket(void)
 {
