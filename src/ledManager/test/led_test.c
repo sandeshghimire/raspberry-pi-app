aaba82ec8fc64b3f3c6632b30bd1dc9906eec5b2
sandesh ghimire <sandeshghimire@outlook.com>
2013-10-11 17:08:23 -0700

    basic changes

--- src/ledManager/test/led_test.c
+++ src/ledManager/test/led_test.c
@@ -1,9 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <syslog.h>
+#include <sched.h>
 
 #include "../led_pub.h"
 
-int main(int argc, char ** argv)
+static int
+daemon_init(void);
+static int process_commands(void);
+
+/*
+ *@name main
+ *@in
+ *@in
+ *@out
+ *@out
+ */
+
+int main(int argc,char ** argv)
 {
-	return 0;
+  daemon_init();
+  for (;;)
+  {
+    process_commands();
+    sleep(10);
+  }
+  return 0;
+}
+
+/*
+ *@name daemon_init
+ *@in
+ *@in
+ *@out
+ *@out
+ */
+static int daemon_init(void)
+{
+  pid_t pid;
+  if (pid = fork() < 0)
+  {
+    return -1;
+  }
+  else if (pid != 0)
+  {
+    exit(0);
+  }
+  setsid();
+  chdir("/");
+  umask(0);
+  return 0;
+}
+
+/*
+ *@name process_commands
+ *@in
+ *@in
+ *@out
+ *@out
+ */
+static int process_commands(void)
+{
+
 }
